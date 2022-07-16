6e621e678d76f86c3c92fd33d0f657a9f76b8c89
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-16 17:59:46 +0200

    refactor layout components to use widget builder

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -12,13 +12,25 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Container(
-          decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
-          padding: const EdgeInsets.all(20.0),
-          margin: const EdgeInsets.all(20.0),
-          child: Image.network(images[index].url),
-        );
+        return buildImage(images[index]);
       },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+      padding: const EdgeInsets.all(20.0),
+      margin: const EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child: Image.network(image.url),
+            padding: EdgeInsets.only(bottom: 10.0),
+          ),
+          Text(image.title)
+        ],
+      ),
     );
   }
 }
