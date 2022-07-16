58f992f8569141ae3cc497d1d5155c38a8b1765f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-16 15:48:16 +0200

    get api, use image model

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,26 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'package:pics/src/models/image_models.dart';
 
 class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
   createState() {
     return AppState();
   }
 }
 
 class AppState extends State<App> {
-  int counter = 0;
+  int counter = 1;
 
+  void fetchImage() async {
+    print('change image');
+    counter++;
+    var uri = 'https://jsonplaceholder.typicode.com/posts/$counter';
+    var response = await get(Uri.parse(uri));
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+  }
+
+  @override
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            setState(() => counter += 1);
-          },
+          onPressed: fetchImage,
+          child: const Icon(Icons.add),
         ),
         appBar: AppBar(
-          title: Text('Lets see some images'),
+          title: const Text('Lets see some images'),
         ),
       ),
     );
